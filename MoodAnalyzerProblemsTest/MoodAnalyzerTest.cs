db6f330c8bfdbe08f5a0aeb1e34049caa8689fa9
Hema <hemasahu019@gmail.com>
2022-08-18 16:55:40 +0530

    Added UC2 code to handle null input using Exception

--- MoodAnalyzerProblemsTest/MoodAnalyzerTest.cs
+++ MoodAnalyzerProblemsTest/MoodAnalyzerTest.cs
@@ -9,7 +9,7 @@ namespace MoodAnalyzerProblemsTest
     {
         [DataRow("I am in Sad Mood", "SAD")] //TC1.1: Given “I am in Sad Mood” message should Return SAD
         [DataRow("I am in Any Mood", "HAPPY")] //TC1.2: Given “I am in Any Mood” message should Return HAPPY
-        [DataRow(null, "HAPPY")]
+        [DataRow(null, "HAPPY")]//TC2.1: Given Null Mood should Return Happy
         [TestMethod]
         public void GivenMessageReturnMood(string message, string expected)
         {
