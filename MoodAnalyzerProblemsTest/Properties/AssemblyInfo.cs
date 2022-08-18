8e3b010898df290f5377ed029b26cba05d1fdc51
Hema <hemasahu019@gmail.com>
2022-08-18 16:23:47 +0530

    Added welcome message to main

---
+++ MoodAnalyzerProblemsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MoodAnalyzerProblemsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoodAnalyzerProblemsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("17d73ee7-54db-41cc-873e-2f52b956fe0e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MoodAnalyzerProjblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProjblem
+{ /// <summary>
+  /// Day 20 :Mood Analyzer Project
+  /// </summary>
+    public class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzer Problems"); //Welcome msg page
+            Console.ReadLine();
+        }
+    }
+}
