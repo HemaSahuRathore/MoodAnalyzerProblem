a76e2ff574423328316917520346163c663a731c
Hema <hemasahu019@gmail.com>
2022-08-18 16:30:15 +0530

    Added UC1 code to repond mood Happy or Sad

---
+++ MoodAnalyzerProblemsTest/MoodAnalyzerTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzerProjblem;
+using System;
+
+namespace MoodAnalyzerProblemsTest
+{
+    [TestClass]
+    public class MoodAnalyzerTest
+    {
+        public MoodAnalyzer moodAnalyzerObj;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            moodAnalyzerObj = new MoodAnalyzer();
+        }
+
+        [DataRow("I am in Sad Mood", "SAD")] //TC1.1
+        [DataRow("I am in Any Mood", "HAPPY")] //TC1.2
+        [TestMethod]
+        public void GivenMessageReturnMood(string message, string expected)
+        {
+            //Act
+            string actual = moodAnalyzerObj.AnalyseMood(message);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ MoodAnalyzerProjblem/MoodAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProjblem
+{ /// <summary>
+  /// UC 1: Given a Message, ability to analyse and respond Happy or Sad Mood, 
+  ///       - Create MoodAnalyser Object 
+  ///       - Call analyseMood function with message as parameter and return Happy or Sad Mood
+  /// TC1.1 : Given “I am in Sad Mood” message should Return SAD
+  /// TC1.2 : Given “I am in Any Mood” message should Return HAPPY
+  /// </summary>
+    public class MoodAnalyzer
+    {
+        //Method to Analyse Mood
+        public string AnalyseMood(string message)
+        {
+            string SAD = "SAD";
+            string HAPPY = "HAPPY";
+
+            if (message.ToUpper().Contains(SAD))
+                return SAD;
+            else
+                return HAPPY;
+
+        }
+    }
+}
